21aeff1ce7e2774ffc1a59ee71dae8c4138d191d
Rhys <121772484+rhysez@users.noreply.github.com>
2025-11-11 21:34:27 +0000

    Initial commit

---
+++ src/main/java/org/example/microdoc/Main.java
@@ -0,0 +1,16 @@
+package org.example.microdoc;
+
+// TODO: Create a Document class
+// TODO: Create a Collection class (has many Documents)
+// TODO: Create a Database class (has many Collections)
+// TODO: Create a Connection class (has one Database)
+
+public class Main {
+    static void main() {
+        IO.println(String.format("Hello and welcome!"));
+
+        for (int i = 1; i <= 5; i++) {
+            IO.println("i = " + i);
+        }
+    }
+}
---
+++ src/main/java/org/example/microdoc/model/Document.java
@@ -0,0 +1,4 @@
+package org.example.microdoc.model;
+
+public class Document {
+}
